4123bf94a7488a5d753be8c950399be12dcd5e03
Austinma10 <austinma2168@gmail.com>
2024-01-25 17:01:24 -0500

    Added more license options and finshed eliminating minor bugs

---
+++ SampleReadMe.md
@@ -0,0 +1,35 @@
+# Sample
+
+  ## Table of Contents
+  #### [Go to Description](#description)
+  #### [Go to Installation](#installation)
+  #### [Go to Usage](#usage)
+  #### [Go to License](#license)
+  #### [Go to Contributing](#contributing)
+  #### [Go to Tests](#tests)
+  #### [Go to Questions](#questions)
+  
+  
+  ## Description
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+  
+  ## Installation
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+  
+  ## Usage
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+  
+  ## License
+  
+  #### ![license badge](https://img.shields.io/badge/License-Apache_2.0-blue.svg)
+  [link to license](https://opensource.org/license/apache-2-0/)
+
+  ## Contributing
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+  
+  ## Tests
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+  
+  ## Questions
+  [My Github page](https://github.com/Austinma1010/readMeGenerator)
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,41 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+  
 let badge;
-if (license == 'Apache License 2.0')  {
-  badge = 'https://img.shields.io/badge/License-Apache_2.0-blue.svg';
-  return badge;
-} }
+if (license == 'Apache License 2.0') {
+  return badge = 'https://img.shields.io/badge/License-Apache_2.0-blue.svg';
+}
+else if (license == 'MIT License') {
+  return badge = 'https://img.shields.io/badge/License-MIT-yellow.svg';
+}
+else if (license == 'The Unlicense') {
+  return badge = 'https://img.shields.io/badge/license-Unlicense-blue.svg';
+}
+else {
+  return;
+}
+
+ }
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
 let link;
 if (license == 'Apache License 2.0') {
-  link = 'https://opensource.org/license/apache-2-0/';
-  return link;
+  return link = 'https://opensource.org/license/apache-2-0/';
 }
+else if (license == 'MIT License') {
+  return link = 'https://opensource.org/licenses/MIT';
 }
+else if (license == 'The Unlicense') {
+  return link = 'http://unlicense.org/';
+}
+else {
+  return;
+}
+
+}
+
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
@@ -22,18 +43,18 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   const {title, description, installation, usageInfo, license, guidLines, testInstructions, gitUrl} = data;
-  let badge = renderLicenseBadge(license);
-  let link = renderLicenseLink(license);
-  return `# ${title}
+  
+  if (license == 'None') {
+    return `# ${title}
 
   ## Table of Contents
-  #### [Go to Description](##description)
-  #### [Go to Installation](##installation)
-  #### [Go to Usage](##usage)
-  #### [Go to License](##license)
-  #### [Go to Contributing](##contributing)
-  #### [Go to Tests](##tests)
-  #### [Go to Questions](##questions)
+  #### [Go to Description](#description)
+  #### [Go to Installation](#installation)
+  #### [Go to Usage](#usage)
+  #### [Go to License](#license)
+  #### [Go to Contributing](#contributing)
+  #### [Go to Tests](#tests)
+  #### [Go to Questions](#questions)
   
   
   ## Description
@@ -46,7 +67,46 @@ function generateMarkdown(data) {
   ${usageInfo}
   
   ## License
-  I have the ${license} license
+  This project doesn't have a license
+
+  ## Contributing
+  ${guidLines}
+  
+  ## Tests
+  ${testInstructions}
+  
+  ## Questions
+  [My Github page](${gitUrl})
+
+`;
+
+  } else {
+
+  let badge = renderLicenseBadge(license);
+  let link = renderLicenseLink(license);
+  return `# ${title}
+
+  ## Table of Contents
+  #### [Go to Description](#description)
+  #### [Go to Installation](#installation)
+  #### [Go to Usage](#usage)
+  #### [Go to License](#license)
+  #### [Go to Contributing](#contributing)
+  #### [Go to Tests](#tests)
+  #### [Go to Questions](#questions)
+  
+  
+  ## Description
+  ${description}
+  
+  ## Installation
+  ${installation}
+  
+  ## Usage
+  ${usageInfo}
+  
+  ## License
+  
   #### ![license badge](${badge})
   [link to license](${link})
 
@@ -59,7 +119,7 @@ function generateMarkdown(data) {
   ## Questions
   [My Github page](${gitUrl})
 
-`;
+`; }
 }
 
 module.exports = generateMarkdown;
